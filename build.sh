e9b6885e16f17858bac9e6dbb05df5ab6bd0f0a6
Marcel <MTRNord@users.noreply.github.com>
2017-01-20 21:02:02 +0100

    Add Docker compatible Build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd /gluon
+make update
+OPTIONS="BUILD_LOG=1 FORCE_UNSAFE_CONFIGURE=1 DEFAULT_GLUON_BRANCH=develop DEFAULT_GLUON_RELEASE=develop-2017.$
+CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
+  echo "Kompiliert wird auf $CORES Kernen"
+for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-64 x86-generic x86-xen_domu; do
+  echo "################# $TARGET ###########################"
+  make GLUON_TARGET=$TARGET $OPTIONS || break
+  echo "############$TARGET fertig ##########################" 
+done && echo "alle Targets wurden erfolgreich erstellt." || echo "Fehlschlag! Abbruch"
