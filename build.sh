5a55f41309b92aad480aeb2d367f054a1ab25055
Marcel <MTRNord@users.noreply.github.com>
2017-02-17 23:41:24 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd /gluon
 make update
-OPTIONS="BUILD_LOG=1 FORCE_UNSAFE_CONFIGURE=1 DEFAULT_GLUON_BRANCH=develop DEFAULT_GLUON_RELEASE=develop-2016.2.2-0 V=s"
+OPTIONS="DEFAULT_GLUON_BRANCH=develop DEFAULT_GLUON_RELEASE=develop-2016.2.3-0 V=s"
 CORES=$(lscpu|grep -e '^CPU(s):'|xargs|cut -d" " -f2)
   echo "Kompiliert wird auf $CORES Kernen"
-for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-64 x86-generic x86-xen_domu; do
+for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-64 x86-generic x86-kvm_guest x86-xen_domu; do
   echo "################# $TARGET ###########################"
   make GLUON_TARGET=$TARGET $OPTIONS || break
   echo "############$TARGET fertig ##########################" 
