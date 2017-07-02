097969826d2628aebea0dd2ac3517ea5b46a07ae
Marcel <MTRNord@users.noreply.github.com>
2017-07-02 22:02:13 +0200

    Update validate_site.sh

--- tests/validate_site.sh
+++ tests/validate_site.sh
@@ -2,4 +2,4 @@
 
 # validate_site.sh checks if the site.conf is valid json
 
-GLUON_SITEDIR="." lua5.1 tests/site_config.lua -v
+GLUON_SITEDIR="." lua5.1 tests/site_config.lua -v && jsonlint ./site.conf -t [    ]
