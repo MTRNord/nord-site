4e1e5dcf2b109f80a349d762737b490003836a2a
Marcel <MTRNord@users.noreply.github.com>
2017-07-02 22:06:40 +0200

    Update site_config.lua

--- tests/site_config.lua
+++ tests/site_config.lua
@@ -1,27 +1,18 @@
 require 'busted.runner'()
 local validJson = require "tests/validJson"
 local config = os.getenv('GLUON_SITEDIR')
-function jsonLint (json)
-      local file = io.open("/tmp/config.json", "a")
-      io.write(json)
-      io.close(file)
-      local handle = assert(io.popen("jsonlint /tmp/config.json -t [    ]"))
-      local result = handle:read("*all")
-      handle:close()
-      print(result)
-end
 
 describe("Test config", function()
       it("should be  a valid config", function()
             local json = io.open(config .. '/site.conf'):read('*a')
             if validJson(json) then
-                  jsonLint(json)
+                  assert.True(true)
             else
-                local errfn = function()
+               local errfn = function()
                   error("Invalid Config: ")
-                end
+               end
 
-                assert.has_error(errfn, "Invalid Config: ")
+               assert.has_error(errfn, "Invalid Config: ")
                local function loader()
                   coroutine.yield('return ')
                   coroutine.yield(io.open(config .. '/site.conf'):read('*a'))
@@ -32,4 +23,6 @@ describe("Test config", function()
             end
      end)
 end)
-
+local file = io.open("/tmp/config.json", "a")
+io.write(io.open(config .. '/site.conf'):read('*a'))
+io.close(file)
